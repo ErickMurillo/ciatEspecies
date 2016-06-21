3f8feb929dab73e18b217bccc958c86239cfff11
Carlos Rocha <crocha09.09@gmail.com>
2016-06-21 17:02:04 -0600

    agregando modelo de grupos focales

--- focusgroups/models.py
+++ focusgroups/models.py
@@ -4,3 +4,24 @@ from django.db import models
 
 # Create your models here.
 
+CHOICE_REGION = (
+					(1, 'APAC'),
+					(2, 'SSA'),
+					(3, 'Mena'),
+					(4, 'LatAm'),
+				)
+
+class Country(models.Model):
+	name = models.CharField(max_length=250)
+	latitud = models.FloatField()
+	longitud = models.FloatField()
+
+class Location(models.Model):
+    """
+    Description: Info about location all focus groups
+    """
+    region = models.IntField(choices=CHOICE_REGION)
+    country = models.ForeignKey(Country)
+    
+
+    
--- species/admin.py
+++ species/admin.py
@@ -4,7 +4,11 @@ from .models import Species
 
 
 class SpeciesAdmin(ImportExportModelAdmin):
-    pass
+    empty_value_display = '-empty-'
+    list_display = ('id','scientific_name', 'food_group', 'cultivar', 'type_species')
+    list_display_links = ('scientific_name', 'food_group')
+    list_filter = ('cultivar', 'type_species')
+    search_fields = ['scientific_name', 'common_name']
 
 # Register your models here.
 admin.site.register(Species, SpeciesAdmin)
