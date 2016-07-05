5f129264020cf43b4f886d4393141b5558c2cc4e
Carlos Rocha <crocha09.09@gmail.com>
2016-07-04 22:51:37 -0600

    add test page

--- ciatEspecies/urls.py
+++ ciatEspecies/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name="index.html")),
     url(r'^jet/', include('jet.urls', 'jet')),
     #url(r'^jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),
     url(r'^admin/', admin.site.urls),
