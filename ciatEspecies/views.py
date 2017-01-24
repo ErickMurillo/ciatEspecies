5c740b4877981d8dc98f7933acc33bafc6ed0182
shiminasai <pablo.ismael90@gmail.com>
2017-01-24 14:18:24 -0600

    martes 24 de enero

--- ciatEspecies/views.py
+++ ciatEspecies/views.py
@@ -28,7 +28,8 @@ def index(request,template="index.html"):
     species = FocusGroup.objects.all().distinct('fcacode__species').count()
     focus_groups = FocusGroup.objects.all().count()
     proyectos = Proyectos.objects.order_by('-id')
-    cientificos = Cientificos.objects.order_by('-id')
+    cien = Scientists.objects.order_by('-id')
+    org = Organizations.objects.order_by('-id')
 
     dicc = {}
     orgs = {}
